d893a4041d78ca08ab6308516472c7cfe6c9e759
Aiden Fox Ivey <aiden@fox-ivey.com>
2023-10-01 16:31:21 -0400

    latest

--- LogicalStep/software/Lab_1/hello_world.c
+++ LogicalStep/software/Lab_1/hello_world.c
@@ -1,59 +1,108 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
-#include "system.h"
 #include "altera_avalon_pio_regs.h"
-
-#define EGM_ENABLE_BIT 1
-#define EGM_DISABLE_BIT 0
+#include "sys/alt_irq.h"
+#include "system.h"
 
 #define ENABLE_REG_OFFSET 0
 #define BUSY_REG_OFFSET 1
 #define PERIOD_REG_OFFSET 2
 #define PULSE_WIDTH_REG_OFFSET 3
-#define	AVERAGE_LATENCY_REG_OFFSET 4
+#define AVERAGE_LATENCY_REG_OFFSET 4
 #define MISSED_REG_OFFSET 5
 #define MULTI_REG_OFFSET 6
 
 void enable_egm(uint16_t period);
 void disable_egm();
+void print_statistics(uint16_t period, uint64_t tasks_run);
+static void response_interrupt(void *context, alt_u32 id);
 uint8_t is_busy();
 
-int main()
-{
-  volatile uint8_t busy_bit = 1;
-  enable_egm(200);
+volatile int triggered = 0;
 
-  while(busy_bit != 0) {
-	  busy_bit = is_busy();
+int main() {
+	// TODO: Name something better than this. -- throw this inside a function
+	// 0 -> interrupts vs 1 -> polling
+	// uint8_t interrupts_or_polling = IORD(SW_BASE, SW_OFFSET);
+	// if (interrupts_or_polling) {}
+	// else {}
 
-	  printf("Busy: %d\n", busy_bit);
-  }
+	puts("Interrupt mode selected.");
+	puts("Tight polling mode selected");
+	puts("Period, Pulse_Width, BG_Tasks Run, Latency, Missed, Multiple");
 
-  printf("Exiting ... \n");
-  disable_egm();
+	puts("Please, press PB0 to continue.");
+
+	while(1) {
+		if (IORD(BUTTON_PIO_BASE, 0) == 1) {
+			break;
+		}
+	}
+
+	puts("made it through");
+
+//	volatile uint8_t busy_bit;
+//
+//	for (uint16_t period = 2; period <= 5000; period += 2) {
+//		busy_bit = 1;
+//		enable_egm(period);
+//
+//		interrupt_routine();
+
+//		print_statistics();
+//		disable_egm();
+//	}
 
   return 0;
 }
 
-void enable_egm(uint16_t period)
-{
+void print_statistics(uint16_t period, uint64_t tasks_run) {
 	uint16_t pulse_width = period / 2;
-
-	// Configure EGM period and pulse width
-	IOWR(EGM_BASE, PERIOD_REG_OFFSET, period);
-	IOWR(EGM_BASE, PULSE_WIDTH_REG_OFFSET, pulse_width);
-
-	// Enable EGM
-	IOWR(EGM_BASE, ENABLE_REG_OFFSET, EGM_ENABLE_BIT);
+	uint16_t latency = IORD(EGM_BASE, AVERAGE_LATENCY_REG_OFFSET);
+	uint16_t missed = IORD(EGM_BASE, MISSED_REG_OFFSET);
+	uint16_t multiple = IORD(EGM_BASE, MULTI_REG_OFFSET);
+	printf("%d, %d, %d, %d, %d, %d\n", period, pulse_width, tasks_run);
 }
 
-void disable_egm()
-{
-	// Disable EGM
-	IOWR(EGM_BASE, ENABLE_REG_OFFSET, EGM_DISABLE_BIT);
+void enable_egm(uint16_t period) {
+  uint16_t pulse_width = period / 2;
+
+  // Configure EGM period and pulse width
+  IOWR(EGM_BASE, PERIOD_REG_OFFSET, period);
+  IOWR(EGM_BASE, PULSE_WIDTH_REG_OFFSET, pulse_width);
+
+  // Enable EGM
+  IOWR(EGM_BASE, ENABLE_REG_OFFSET, 1);
 }
 
-uint8_t is_busy() {
-	return IORD(EGM_BASE, BUSY_REG_OFFSET);
+void disable_egm() { IOWR(EGM_BASE, ENABLE_REG_OFFSET, 0); }
+uint8_t is_busy() { return IORD(EGM_BASE, BUSY_REG_OFFSET); }
+
+void interrupt_routine() {
+	alt_irq_register(STIMULUS_IN_IRQ, (void*)0, response_interrupt);
+
+	while (is_busy()) {
+		if (triggered == 1) {
+		    IOWR(RESPONSE_OUT_BASE, 0, 1);
+		    IOWR(RESPONSE_OUT_BASE, 0, 0);
+			triggered = 0;
+		}
+	}
 }
+
+static void response_interrupt(void *context, alt_u32 id) {
+    triggered = 1;
+}
+
+void tight_polling() {
+  //	characterize before we get to this point
+
+  while (is_busy()) {
+    if (IORD(STIMULUS_IN_BASE, 0) == 1) {
+      IOWR(RESPONSE_OUT_BASE, 0, 1);
+      IOWR(RESPONSE_OUT_BASE, 0, 0);
+    }
+  }
+}
+
